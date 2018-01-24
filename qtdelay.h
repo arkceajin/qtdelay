f7a4a19a88a6665bf3bea5cf5c791d3d09e3542b
arkceajin <arkceajin@gmail.com>
2018-01-24 11:20:27 +0900

    base

---
+++ qtdelay.h
@@ -0,0 +1,23 @@
+#ifndef QTDELAY_H
+#define QTDELAY_H
+
+#include <QTimer>
+
+/**
+  * @brief execute a part of the code after the specified delay.
+  *
+  * @param time_ delay
+  * @param todo_ function block
+  *
+  * @code{.cpp}
+  * DelayTodo(1000, {qDebug()<<"Hello";});
+  * @endcode
+  */
+#define QtDelay(time_, todo_)\
+QTimer* delay_ = new QTimer;\
+delay_->setSingleShot(true);\
+QObject::connect(delay_, &QTimer::timeout, [delay_](){ todo_; delay_->deleteLater();});\
+delay_->start(time_)
+
+
+#endif // QTDELAY_H
